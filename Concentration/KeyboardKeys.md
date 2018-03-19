70b69c086901ad8c74f791a36e87500b561eb726
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-03-19 19:23:54 +0300

    Added Basic if statement function to a button

---
+++ Concentration/KeyboardKeys.md
@@ -0,0 +1,10 @@
+#  KeyBoard Keys
+
+## Xcode Keyboard Shortcuts
+```
+*   CTRL + Drag to your code        // To code an UI item
+
+*   ALT + Click         // To view the documentation
+
+*
+```
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+//        print("Hi Ghost")
+        flipCard(withEmoji: "👻", on: sender)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        }
     }
-
-
+    
 }
 
